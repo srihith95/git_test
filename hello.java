f8874c8c8fe4febdf32d17cc5149d6d69eaeaa5e
srihith95 <103873053+srihith95@users.noreply.github.com>
2022-04-20 20:53:05 +0530

    Add hello.java

---
+++ hello.java
@@ -0,0 +1,8 @@
+class hello{
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        System.out.println("This is first Java program as a commit, so please bear with me if I'm annoying:)");
+        int a=2,b=3;
+        System.out.println("a+b="+a+b);
+    }
+}
