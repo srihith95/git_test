87f807b6cbd6e3a094054ea0afb29458f08c98cb
srihith95 <103873053+srihith95@users.noreply.github.com>
2022-04-20 20:42:06 +0530

    Delete hello.java

--- hello.java
+++
@@ -1,6 +0,0 @@
-class hello{
-    public static void public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.print("This is my first Java prgram which is being pushed into git, please bear with me if I'm annoying:)");
-    }
-}
